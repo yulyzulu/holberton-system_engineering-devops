2a1f78eb75b8000115f179c83eff493b0381e4ab
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-03-25 02:28:00 +0000

    Added task 4

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,10 +1,11 @@
+#!/usr/bin/env bash
 # SSH configuration file so that you can connect to a server without typing a password
 file_line { 'holberton':
-  path => 'etc/ssh/ssh_config',
+  path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/holberton',
 }
 
 file_line { 'password':
-  path => 'etc/ssh/ssh_config',
+  path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
 }
