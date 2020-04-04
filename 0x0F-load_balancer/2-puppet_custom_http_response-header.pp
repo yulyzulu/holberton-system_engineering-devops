0303c9dd3123d0619cc832a7ba0f78b20c415994
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-04-04 03:25:09 +0000

    Added script to configure servers

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,5 +1,5 @@
 # Configure Nginx so that its HTTP response contains a custom header
 exec { 'conf_server':
-    command  => sudo apt-get update&&sudo apt-get -y install nginx&&echo "Holberton School" | sudo tee /var/www/html/index.html&&sudo sed -i "/server_name _;/a rewrite ^/redirect_me https://twitter.com/Yulyzulu5 permanent;" /etc/nginx/sites-available/default&&sudo sed -i "11 a \ \tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf&&sudo service nginx restart,
+    command  => 'sudo apt-get update&&sudo apt-get -y install nginx&&echo "Holberton School" | sudo tee /var/www/html/index.html&&sudo sed -i "/server_name _;/a rewrite ^/redirect_me https://twitter.com/Yulyzulu5 permanent;" /etc/nginx/sites-available/default&&sudo sed -i "11 a \ \tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf&&sudo service nginx restart',
     provider => shell,
 }
